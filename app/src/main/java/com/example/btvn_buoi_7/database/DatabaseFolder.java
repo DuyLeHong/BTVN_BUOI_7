fab20827c81253f218d23666334168c5c5144793
Duy Le <lehongduy.bk@gmail.com>
2022-03-11 21:13:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/btvn_buoi_7/database/DatabaseFolder.java
@@ -0,0 +1,83 @@
+package com.example.btvn_buoi_7.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.example.btvn_buoi_7.folder.FolderModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseFolder extends SQLiteOpenHelper {
+
+
+    private static final String DATABASE_NAME = "myfolder.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "folder";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_DESCRIPTION = "description";
+
+    public DatabaseFolder(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "(" +
+                KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                KEY_NAME + " VACHAR(200)," +
+                KEY_DESCRIPTION + "  VACHAR(200) )";
+
+
+        sqLiteDatabase.execSQL(sql);
+
+        Log.d("AAA", "create table");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    }
+
+    public List<FolderModel> getAllFolder() {
+        List<FolderModel> listFolder = new ArrayList<>();
+
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM " + TABLE_NAME + "", null);
+        while (data.moveToNext()) {
+            int id = data.getInt(data.getColumnIndex(KEY_ID));
+            String name = data.getString(data.getColumnIndex(KEY_NAME));
+            String description = data.getString(data.getColumnIndex(KEY_DESCRIPTION));
+            listFolder.add(new FolderModel(id, name, description));
+        }
+        return listFolder;
+    }
+
+    public void insertFolder(FolderModel folder) {
+        SQLiteDatabase db = getWritableDatabase();
+        //db.execSQL("INSERT INTO "+TABLE_NAME+"(name,description) VALUES (?,?)",new String[]{folder.getName(),folder.getDescription()});
+        db.execSQL("INSERT INTO folder(name,description) VALUES ('" + folder.getName() + "','" + folder.getDescription()+"')");
+        Log.d("AAA", "thêm mới dữ liệu thành công");
+
+    }
+
+    public void updateFolder(FolderModel folder) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("UPDATE " + TABLE_NAME + " SET " +
+                KEY_NAME + " = ?," +
+                KEY_DESCRIPTION + " = ? WHERE " +
+                KEY_ID + " = ?", new String[]{folder.getName(), folder.getDescription(), folder.getId() + ""});
+    }
+
+    public void deleteFolder(FolderModel folder) {
+        SQLiteDatabase db = getWritableDatabase();
+        String sql = "DELETE FROM " + TABLE_NAME + " WHERE " + KEY_ID + "=? ";
+        db.execSQL(sql, new String[]{folder.getId() + ""});
+    }
+}
---
+++ app/src/main/java/com/example/btvn_buoi_7/folder/Activity_Add_Edit_Folder.java
@@ -0,0 +1,148 @@
+package com.example.btvn_buoi_7.folder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.btvn_buoi_7.R;
+
+import java.util.List;
+
+public class Activity_Add_Edit_Folder extends AppCompatActivity {
+    TextView tv_cancel, tv_save,tv_title;
+    EditText edt_name, edt_description;
+    List<FolderModel> mlistFolder;
+    String name, description;
+    int index;
+    FolderModel folder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_folder);
+
+        tv_title = findViewById(R.id.tv_title);
+        tv_cancel = findViewById(R.id.tv_cancel);
+        tv_save = findViewById(R.id.tv_save);
+        edt_name = findViewById(R.id.edt_name);
+        edt_description = findViewById(R.id.edt_description);
+
+        setupToolbar();
+
+        Bundle bundle = getIntent().getExtras();
+        mlistFolder = (List<FolderModel>) getIntent().getSerializableExtra(Activity_Folder.LIST_FOLDER);
+        folder = (FolderModel) bundle.getSerializable(Activity_Folder.ITEM_FOLDER);
+
+        if(getIntent().getAction() == Activity_Folder.ACTION_ADD){
+            AddActivity();
+        }if (getIntent().getAction()== Activity_Folder.ACTION_EDIT){
+            EditActivity();
+        }
+
+    }
+
+    private void AddActivity(){
+        tv_title.setText(getString(R.string.add_folder));
+        tv_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItemFoler();
+            }
+        });
+
+        tv_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void EditActivity(){
+        tv_title.setText(getString(R.string.edit_folder));
+
+        edt_name.setText(folder.getName());
+        edt_description.setText(folder.getDescription());
+
+        tv_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    updateItemFoler();
+            }
+        });
+
+        tv_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void addItemFoler() {
+        name = edt_name.getText().toString().trim();
+        description = edt_description.getText().toString().trim();
+        if (checkFolderName(name)) {
+            Toast.makeText(this, getString(R.string.name_folder_exists), Toast.LENGTH_SHORT).show();
+        } else if (name.equals("")) {
+            Toast.makeText(this, getString(R.string.name_folder_not_empty), Toast.LENGTH_SHORT).show();
+        } else if (description.equals("")) {
+            Toast.makeText(this,  getString(R.string.discription_folder_exists), Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = getIntent();
+            intent.putExtra(Activity_Folder.NAME_FOLDER,name);
+            intent.putExtra(Activity_Folder.DESCRIPTION_FOLDER, description);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+    private void updateItemFoler() {
+
+        String name = edt_name.getText().toString().trim();
+        String description = edt_description.getText().toString().trim();
+
+        if (name.equals("")) {
+            Toast.makeText(this, getString(R.string.name_folder_not_empty), Toast.LENGTH_SHORT).show();
+        } else if (description.equals("")) {
+            Toast.makeText(this, getString(R.string.discription_folder_exists), Toast.LENGTH_SHORT).show();
+        }  else if (name.equals(folder.getName()) && description.equals(folder.getDescription())) {
+            Toast.makeText(this, getString(R.string.name_discription_not_change), Toast.LENGTH_SHORT).show();
+        }else if(checkFolderName(name)){
+            Toast.makeText(this, getString(R.string.name_folder_exists), Toast.LENGTH_SHORT).show();
+        }else {
+            folder.setName(name);
+            folder.setDescription(description);
+            Intent intent = new Intent(this, Activity_Folder.class);
+            Bundle bundle = new Bundle();
+
+            bundle.putSerializable(Activity_Folder.ITEM_FOLDER,folder);
+            intent.putExtras(bundle);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+
+    private boolean checkFolderName(String name) {
+        for (FolderModel mfolder: mlistFolder) {
+            if(folder != null){
+                if(mfolder.getId() == folder.getId() ){
+                    continue;
+                }
+            }
+            if (name.equals(mfolder.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void setupToolbar(){
+        getWindow().setStatusBarColor(getColor(R.color.white));
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+}
